b74eb280d59644df57ef7e090f6dc1352a3b0c9a
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-12-04 10:59:44 -0800

    completed mini test app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
@@ -24,5 +24,12 @@ export class AppComponent {
     });
   }
 
+  onChangeFirst(){
+    this.serverElements[0].name = "Changed!"
+  }
+
+  onDestroyFirst(){
+    this.serverElements.splice(0, 1)
+  }
 
 }
